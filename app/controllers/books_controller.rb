e2031bba2ef7e545b80d5faaf6de1672463c2f23
EC2 Default User <ec2-user@ip-172-31-20-97.ec2.internal>
2022-05-11 15:28:36 +0000

    [Update]次は投稿詳細画面をやりたい

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,21 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    book = Book.new
+    book.save
+    redirect_to index
+  end
+
   def show
   end
 
   def edit
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:list).permit(:title, :body)
   end
 end
